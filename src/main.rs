43fcb14a87870478f1f9b1d44afee5a54af69560
Alex Ballmer <gnu3ra@riseup.net>
2022-08-09 23:15:04 -0500

    better organization for templating code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
 use clap::Parser;
 use std::path::PathBuf;
+use template::{ARGS, TERA};
 use utils::library_path_contains;
-
 pub(crate) mod template;
 pub(crate) mod utils;
 
@@ -19,27 +19,35 @@ struct Args {
     /// Alternative templates directory
     templates_dir: Option<PathBuf>,
 
+    /// allow access to the current working directory
+    #[clap(takes_value = false, short, long)]
+    cwd: bool,
+
+    /// List of directories to allow
+    #[clap(short, long)]
+    dir: Vec<PathBuf>,
+
     /// Arguments to pass to the executable
+    #[clap(last = true)]
     args: Vec<String>,
 }
 
-fn run_sb(args: Args) -> Result<()> {
+fn run_sb() -> Result<()> {
     let library_path = utils::get_library_path()?;
     utils::populate_sb_tree(&utils::SANDBOX_DIR, &library_path)?;
 
-    let profile = template::run_template(&template::TERA, &args.name)?;
-    utils::sandbox_exec(&profile, &args.exe, &args.args)?;
+    let profile = TERA.run_template(&ARGS.name)?;
+    utils::sandbox_exec(&profile, &ARGS.exe, &ARGS.args)?;
 
     Ok(())
 }
 
 fn main() -> Result<()> {
-    let args = Args::parse();
-    if utils::SANDBOX_DIR.contains(&args.name) || library_path_contains(&args.name)? {
-        run_sb(args)?;
+    TERA.get_tera().unwrap();
+    if utils::SANDBOX_DIR.contains(&ARGS.name) || library_path_contains(&ARGS.name).unwrap() {
+        crate::run_sb().unwrap();
     } else {
-        println!("config {} doesn't exist", args.name);
+        println!("config {} doesn't exist", ARGS.name);
     }
-
     Ok(())
 }
--- src/template/mod.rs
+++ src/template/mod.rs
@@ -1,37 +1,83 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, path::PathBuf};
 
-use crate::utils;
-use once_cell::sync::Lazy;
-use tera::{Context, Tera, Value};
+use crate::{utils, Args};
+use anyhow::Result;
+use clap::Parser;
+use once_cell::sync::{Lazy, OnceCell};
+use tera::{Context, Function, Tera, Value};
 use whoami::username;
 
-pub(crate) static TERA: Lazy<Tera> = Lazy::new(|| {
-    let library_glob = utils::get_library_path()
-        .expect("failed to get library path")
-        .join("**.sb")
-        .to_str()
-        .expect("invalid UTF-8 in library path")
-        .to_owned();
+pub(crate) static ARGS: Lazy<Args> = Lazy::new(|| Args::parse());
 
-    let mut tera = Tera::new(&library_glob).expect("failed to initialize Tera");
-    tera.register_function("include", include);
-    tera
-});
+pub(crate) static TERA: Lazy<SandboxTemplate> = Lazy::new(|| SandboxTemplate::new(&ARGS));
 
-/// Render a single template
-pub(crate) fn run_template<T: AsRef<str>>(tera: &Tera, template: T) -> Result<String, tera::Error> {
-    let mut context = Context::new();
-    context.insert("username", &username());
-    tera.render(template.as_ref(), &context)
+fn bind<F>(func: F) -> impl Function
+where
+    F: Fn(&HashMap<String, Value>) -> Result<Value, tera::Error> + Send + Sync,
+{
+    func
 }
 
-/// Handler for tera "include" function. Renders and inserts an additional template
-fn include(path: &HashMap<String, Value>) -> Result<Value, tera::Error> {
-    let template = path
-        .get("path")
-        .ok_or("invalid name")?
-        .as_str()
-        .ok_or("failed")?;
+pub(crate) struct SandboxTemplate<'a> {
+    args: &'a Args,
+    tera: OnceCell<Tera>,
+}
+impl SandboxTemplate<'static> {
+    pub(crate) fn new(args: &'static Args) -> Self {
+        Self {
+            args,
+            tera: OnceCell::new(),
+        }
+    }
 
-    run_template(&TERA, template).map(Value::String)
+    pub(crate) fn get_tera(&'static self) -> Result<()> {
+        let library_glob = utils::get_library_path()
+            .expect("failed to get library path")
+            .join("**.sb")
+            .to_str()
+            .expect("invalid UTF-8 in library path")
+            .to_owned();
+
+        let mut tera = Tera::new(&library_glob).expect("failed to initialize Tera");
+        let f = bind(|path| self.include(path));
+        tera.register_function("include", f);
+        self.tera.set(tera).unwrap();
+        Ok(())
+    }
+
+    pub(crate) fn get_dirs_list(&self) -> Result<Vec<PathBuf>> {
+        let mut res = self.args.dir.clone();
+        if self.args.cwd {
+            let cwd = std::env::current_dir()?;
+            res.push(cwd);
+        }
+        Ok(res)
+    }
+
+    /// Render a single template
+    pub(crate) fn run_template<T: AsRef<str>>(&self, template: T) -> Result<String> {
+        let mut context = Context::new();
+        context.insert("username", &username());
+
+        self.get_dirs_list()?;
+        let template = self
+            .tera
+            .get()
+            .unwrap()
+            .render(template.as_ref(), &context)?;
+        Ok(template)
+    }
+
+    /// Handler for tera "include" function. Renders and inserts an additional template
+    fn include(&self, path: &HashMap<String, Value>) -> Result<Value, tera::Error> {
+        let template = path
+            .get("path")
+            .ok_or("invalid name")?
+            .as_str()
+            .ok_or("failed")?;
+
+        self.run_template(template)
+            .map(Value::String)
+            .map_err(|e| tera::Error::msg(e))
+    }
 }
